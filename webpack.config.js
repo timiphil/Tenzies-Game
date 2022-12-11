8d3c2beb10ffbed1490624c27e58c6eb4c7ce03a
Rotimi Ishola <74196357+Timiphil@users.noreply.github.com>
2022-12-11 06:05:48 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "output": {
+    "filename": "[name].pack.js"
+  },
+  "module": {
+    "rules": [
+      {
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "babel-preset-env",
+              "babel-preset-react"
+            ],
+            "plugins": [
+              "babel-plugin-transform-object-rest-spread"
+            ] 
+          }
+        },
+        "exclude": /node_modules/,
+        "test": /\.js$/
+      }
+    ]
+  },
+  "entry": {
+    "index": "./index"
+  }
+};
